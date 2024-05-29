4782c48eaa99a233d51837b00fb417541f19d325
Drishtant AKA DRIX10 <drixisgod@gmail.com>
2024-05-29 12:01:24 +0530

    Added: `discord-moderation-bot/src/commands/mute.js`

---
+++ discord-moderation-bot/src/commands/mute.js
@@ -0,0 +1,17 @@
+```javascript
+// mute.js
+
+// Import necessary libraries and modules here
+
+// Define the mute command function
+const muteCommand = (message, args) => {
+    // Implement the mute command logic here
+};
+
+// Export the mute command function
+module.exports = {
+    name: 'mute',
+    description: 'Mute a user in the server',
+    execute: muteCommand
+};
+```
